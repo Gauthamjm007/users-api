30a38eff94818caf6afb672a29847d6a98c34ca5
harshasavanth <savanthharsha@github.com>
2021-05-16 22:19:24 +0530

    commit

--- utils/errors/rest_errors.go
+++ utils/errors/rest_errors.go
@@ -1,6 +1,9 @@
 package errors
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
 
 type RestErr struct {
 	Message string `json:"message"`
@@ -15,10 +18,22 @@ func NewBadRequestError(message string) *RestErr {
 		Error:   "bad_request",
 	}
 }
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
 func NewNotFoundError(message string) *RestErr {
 	return &RestErr{
 		Message: message,
 		Status:  http.StatusNotFound,
 		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
 	}
 }
